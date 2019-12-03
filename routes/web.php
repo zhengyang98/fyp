b54d2ae791efccd75ca9c1f899d99ab01b095db2
Chung Zheng Yang <czyang98@hotmail.com>
2019-12-03 19:47:42 +0800

    inital setup

--- routes/web.php
+++ routes/web.php
@@ -19,14 +19,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
